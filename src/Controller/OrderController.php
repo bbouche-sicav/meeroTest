a0c695c047673caf3b8a443a6ea537dd780d697d
Benjamin <benjamin.bouche@takit.io>
2019-05-17 16:53:34 +0200

    debut dev creation controller, route via annotaiton et  vue twig associée

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,9 +2,12 @@
 // src/Controller/OrderController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\HttpFoundation\Request;
 
 class OrderController extends AbstractController
 {
@@ -13,8 +16,31 @@ class OrderController extends AbstractController
     */
     public function index()
     {
+        $order = new Order();
+        $order->setOrderId('');
+        $order->setOrderTax('');
+        $order->setMarketplace('');
+        $order->setIdFlux('');
 
-        return $this->render('order/index.html.twig', [
-        ]);
+        $form = $this->createFormBuilder($order)
+            ->add('orderId', TextType::class)
+            ->add('orderAmount', IntegerType::class)
+            ->add('orderTax', IntegerType::class)
+            ->add('marketPlace', TextType::class)
+            ->add('idFlux', IntegerType::class)
+            ->add('save', SubmitType::class, ['label' => 'Créer une nouvelle commande'])
+           ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $order = $form->getData();
+
+        return $this->redirectToRoute('order_success');
+    }
+
+    return $this->render('order/index.html.twig', [
+        'form' => $form->createView(),
+    ]);
     }
 }
